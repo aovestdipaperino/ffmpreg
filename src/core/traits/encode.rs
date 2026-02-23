b37e31f014febe07b2f59045bd4ab418ad08a598
Yazalde Filimone <yazaldefilimon@gmail.com>
2026-02-23 10:17:33 +0200

    *

--- src/core/traits/encode.rs
+++ src/core/traits/encode.rs
@@ -1,8 +1,10 @@
 use crate::core::frame::Frame;
 use crate::core::packet::PacketIter;
+use crate::core::track::TrackFormat;
 use crate::message::Result;
 
 pub trait Encoder {
+	fn input_format(&self) -> TrackFormat;
 	fn encode(&mut self, frame: Frame) -> Result<PacketIter>;
 	fn finish(&mut self) -> Result<PacketIter>;
 }
