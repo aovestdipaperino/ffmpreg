a80b3f8f87ea760381f228b0de04da8d7e78ff5e
Yazalde Filimone <yazaldefilimon@gmail.com>
2026-02-23 10:17:33 +0200

    *

--- src/core/traits/swresample.rs
+++
@@ -1,7 +0,0 @@
-use crate::core::frame::Frame;
-use crate::message::Result;
-
-trait Swresample {
-	fn new(input: &Frame, output: &Frame) -> Self;
-	fn resample(&mut self, input: &Frame, output: &mut Frame) -> Result<()>;
-}
