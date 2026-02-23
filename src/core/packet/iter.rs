aeaa9d860475a8ad4a204cd0c6f9f08ce47a1297
Yazalde Filimone <yazaldefilimon@gmail.com>
2026-02-23 10:17:33 +0200

    *

--- src/core/packet/iter.rs
+++ src/core/packet/iter.rs
@@ -1,23 +1,29 @@
 use crate::core::packet::Packet;
 
 pub struct PacketIter {
-	packets: Vec<Packet>,
+	inner: std::vec::IntoIter<Packet>,
 }
 
 impl PacketIter {
 	pub fn new(packets: Vec<Packet>) -> Self {
-		Self { packets }
+		Self { inner: packets.into_iter() }
 	}
 
 	pub fn empty() -> Self {
-		Self { packets: Vec::new() }
+		Self { inner: Vec::new().into_iter() }
 	}
 }
 
 impl Iterator for PacketIter {
 	type Item = Packet;
 
+	#[inline(always)]
 	fn next(&mut self) -> Option<Self::Item> {
-		self.packets.pop()
+		self.inner.next()
+	}
+
+	#[inline(always)]
+	fn size_hint(&self) -> (usize, Option<usize>) {
+		self.inner.size_hint()
 	}
 }
