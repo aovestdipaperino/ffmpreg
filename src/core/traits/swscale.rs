0e91b4ca93791d867a1a7613bc67f460e156d0ab
Yazalde Filimone <yazaldefilimon@gmail.com>
2026-02-23 10:17:33 +0200

    *

--- src/core/traits/swscale.rs
+++
@@ -1,7 +0,0 @@
-use crate::core::frame::Frame;
-use crate::message::Result;
-
-pub trait Swscale {
-	fn new(input: &Frame, output: &Frame) -> Self;
-	fn scale(&mut self, input: &Frame, output: &mut Frame) -> Result<()>;
-}
